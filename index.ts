59a9602686c39a81a125f229cea896f5361b813c
Ken Chan <chankakit1996@yahoo.com.hk>
2024-09-19 16:06:23 +0800

    inital setup with express and react, redux

--- index.ts
+++ index.ts
@@ -1,3 +1,30 @@
-import { a } from "./a";
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import routes from "./routes";
+dotenv.config();
 
-console.log(a);
+const app = express();
+
+app.use(cors()); // Enable CORS
+app.use(helmet()); // Secure the app by setting various HTTP headers
+app.use(express.json()); // Parse JSON bodies in requests
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies in requests
+
+app.use(routes);
+
+const PORT = process.env.PORT;
+
+mongoose
+  .connect(process.env.DB_URI as string)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
